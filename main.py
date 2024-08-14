792e0a383a5872d4e5fe229281d1d8d883523dc3
Angad Basandrai <angadbasandrai@gmail.com>
2024-08-14 11:47:21 +0530

    feat: added more data and fixed end of season standings

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class Driver:
-    def __init__(self, name, rating=1500):
+    def __init__(self, name, rating=1000):
         self.name = name
         self.rating = rating
         self.history = []
@@ -12,10 +12,10 @@ class Driver:
         self.k = 1
     
     def ratingAdjust(self, scored, expected):
-        if scored >= expected*18/25:
-            self.buffer = self.rating + self.k * (scored-(expected*18/25))
+        if scored >= expected*21/25:
+            self.buffer = self.rating + self.k * (scored-(expected*20/25))
         else:
-            self.buffer = self.rating + self.k * (scored-(expected*22/25))
+            self.buffer = self.rating + self.k * (scored-(expected))
     def upload(self):
         if self.retired:
             self.history.append(np.nan)
@@ -29,14 +29,14 @@ class Driver:
     def effRating(self):
         return self.buffer if not self.retired else self.history[np.where(~np.isnan(self.history) == True)[0][-1]]
     def peakRating(self):
-        return np.nanmax(self.history+[self.rating]) if self.started else 1500
+        return np.nanmax(self.history+[self.rating]) if self.started else 1000
     def __repr__(self):
         if self.started:
-            return self.name + ": 1500 -> " + str(int(self.effRating()*10)/10) + "  ("+ str(int((self.effRating()-1500)*10)/10) +")" + "  Peak: " + str(int(self.peakRating() * 10)/10) + "  (" + str(int((self.effRating() - self.peakRating()) * 10)/10) + ")"
+            return self.name + ": 1000 -> " + str(int(self.effRating()*10)/10) + "  ("+ str(int((self.effRating()-1000)*10)/10) +")" + "  Peak: " + str(int(self.peakRating() * 10)/10) + "  (" + str(int((self.effRating() - self.peakRating()) * 10)/10) + ")"
         else:
             return self.name + ": hasn't made professional debut"
 
-points = [25,18,15,12,10,8,6,4,2,1,-1,-2,-4,-6,-8,-10,-12,-15,-18,-25,-32,-39]
+points = [25,18,15,12,10,8,6,4,2,1,-1,-2,-4,-6,-8,-10,-12,-15,-18,-25,-32,-39,-46,-53,-60,-67,-74,-81,-89]
 
 drivers = []
 f = open('drivers.txt')
@@ -45,20 +45,26 @@ for d in lines:
     drivers.append(Driver(d[:-1]))
 f.close()
 
+y = 2012
+
 f = open('data')
+z = open('winners.txt', 'w')
 lines = f.readlines()
 for n in range(len(lines)):
     s = lines[n].split(',')
+    p = []
     if s[0] == "--":
         for i in range(len(drivers)):
             try:
-                rank = s.index(drivers[i].name)
-                drivers[i].buffer = drivers[i].rating + (drivers[i].k*(11-rank))
+                if s.index(drivers[i].name):
+                    p.append([drivers[i].name, drivers[i].effRating()])
             except:
                 continue
-        for driver in drivers:
-            driver.upload()
-    elif s[0] == "~":
+        p = sorted(p,key = lambda x: x[1], reverse=True)
+        z.write(str(y) + ": ")
+        z.write(str(p) + "\n")
+        y += 1
+    if s[0] == "~":
         for i in range(len(drivers)):
             try:
                 if s.index(drivers[i].name):
@@ -85,7 +91,7 @@ for n in range(len(lines)):
                             continue
                 opp /= n
                 ratings.sort(reverse=True)
-                expected = len(ratings)+1
+                expected = len(ratings)+2
                 score = points[s.index(drivers[i].name)]
                 for l in range(len(ratings)):
                     if ratings[l] <= drivers[i].rating:
@@ -104,6 +110,7 @@ for driver in drivers:
 high = np.nanmax(drivers[0].history)
 drivers = sorted(drivers, key=lambda x: x.effRating(), reverse=True)
 fig, ax = plt.subplots()
+z.close()
 for driver in drivers:
     z = driver.history
     if driver.started:
@@ -114,18 +121,19 @@ for driver in drivers:
 plt.xlabel('races')
 plt.ylabel('elo')
 ax.set_yticks(np.arange(int(np.nanmin(drivers[-1].history))-20, int(high)+20, 20))
-x = np.arange(0,214,1)
-xlabels = ['14','','','','','','','','','','','','','','','','','','','','15','','','','','','','','','','','','','','','','','','','','16','','','','','','','','','','','','','','','','','','','','','17','','','','','','','','','','','','','','','','','','','','','18','','','','','','','','','','','','','','','','','','','','','','19','','','','','','','','','','','','','','','','','','','','','','20','','','','','','','','','','','','','','','','','','21','','','','','','','','','','','','','','','','','','','','','','','22','','','','','','','','','','','','','','','','','','','','','','','23','','','','','','','','','','','','','','','','','','','','','','','24']
+xlabels = ['12','','','','','','','','','','','','','','','','','','','','13','','','','','','','','','','','','','','','','','','','14','','','','','','','','','','','','','','','','','','','15','','','','','','','','','','','','','','','','','','','16','','','','','','','','','','','','','','','','','','','','','17','','','','','','','','','','','','','','','','','','','','18','','','','','','','','','','','','','','','','','','','','','19','','','','','','','','','','','','','','','','','','','','','20','','','','','','','','','','','','','','','','','21','','','','','','','','','','','','','','','','','','','','','','22','','','','','','','','','','','','','','','','','','','','','','23','','','','','','','','','','','','','','','','','','','','','','24','','','','','','','','','','','','','']
+x = np.arange(0,len(xlabels),1)
 ax.set_xticks(x)
 ax.set_xticklabels(xlabels)
-plt.legend(loc="lower right",bbox_to_anchor=(1.1, 0), fontsize="7")
+plt.legend(loc="lower right",bbox_to_anchor=(1.1, -0.1), fontsize="6")
 #plt.tight_layout()
 ax = plt.gca()
 for tick in ax.get_yticks():
     ax.axhline(y=tick, color='gray', linestyle='--', linewidth=0.7)
-ax.axhline(y=1600, color='black', linestyle = '--', linewidth=1.7)
-ax.axhline(y=1500, color='black', linestyle = 'dotted', linewidth=3.7)
-ax.axhline(y=1400, color='black', linestyle = '--', linewidth=1.7)
+ax.axhline(y=1200, color='black', linestyle = '--', linewidth=1.7)
+ax.axhline(y=1100, color='black', linestyle = '--', linewidth=1.7)
+ax.axhline(y=1000, color='black', linestyle = 'dotted', linewidth=3.7)
+ax.axhline(y=900, color='black', linestyle = '--', linewidth=1.7)
 for tick, label in zip(x, xlabels):
     if len(label) > 0:
         ax.axvline(x=tick, color='gray', linestyle='-', linewidth=1)
