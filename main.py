66ff138656129f0c2da2d7b6f79134e27b31bc7f
Angad Basandrai <angadbasandrai@gmail.com>
2024-09-18 13:57:21 +0530

    feat: added sprint format elo

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ class Driver:
     def addHistory(self, history):
         self.history = history
     
-    def ratingAdjust(self, scored, expected):
+    def ratingAdjust(self, scored, expected, k = 1):
             if self.races <= 10:
                 if scored-expected > 0:
-                    self.buffer = self.rating + (scored-(expected)) * 2
+                    self.buffer = self.rating + (scored-(expected)) * 2 * k
                 else:
-                    self.buffer = self.rating + (scored-(expected))
+                    self.buffer = self.rating + (scored-(expected)) * k
             elif self.rating > 1500:
-                self.buffer = self.rating + (scored-(expected)) * 0.5
+                self.buffer = self.rating + (scored-(expected)) * 0.5 * k
             else:
-                self.buffer = self.rating + (scored-(expected))
+                self.buffer = self.rating + (scored-(expected)) * k
             if self.buffer < 1000:
                 self.buffer = 1000
     def upload(self):
@@ -48,18 +48,18 @@ class Driver:
         try:
             return self.buffer if not self.retired else self.history[np.where(~np.isnan(self.history) == True)[0][-1]]
         except:
-            return 1200
+            return 1400
     def peakRating(self):
-        return np.nanmax(self.history+[self.rating]) if self.started else 1200
+        return np.nanmax(self.history+[self.rating]) if self.started else 1400
     def __repr__(self):
         if self.started:
-            return self.name + ": 1200 -> " + str(int(self.effRating()*10)/10) + "  ("+ str(int((self.effRating()-1200)*10)/10) +")" + "  Peak: " + str(int(self.peakRating() * 10)/10) + "  (" + str(int((self.effRating() - self.peakRating()) * 10)/10) + ")"
+            return self.name + ": 1400 -> " + str(int(self.effRating()*10)/10) + "  ("+ str(int((self.effRating()-1400)*10)/10) +")" + "  Peak: " + str(int(self.peakRating() * 10)/10) + "  (" + str(int((self.effRating() - self.peakRating()) * 10)/10) + ")"
         else:
             return self.name + ": hasn't made professional debut"
 
-points = [25,18,15,12,10,8,6,4,2,1,-1,-2,-4,-6,-8,-10,-12,-15,-18,-25,-26,-27,-28,-29,-30]
 
 def recalculate():
+    points = [25,18,15,12,10,8,6,4,2,1,-1,-2,-4,-6,-8,-10,-12,-15,-18,-25,-26,-27,-28,-29,-30]
     drivers = []
     f = open('drivers.csv', 'r')
     lines = f.read().split(',')
@@ -164,7 +164,7 @@ def recalculate():
     for driver in drivers:
         print(driver)
     high = np.nanmax(drivers[0].history)
-    drivers = sorted(drivers, key=lambda x: x.wins, reverse=True)
+    drivers = sorted(drivers, key=lambda x: x.effRating(), reverse=True)
     fig, ax = plt.subplots()
     z.close()
     _names = []
@@ -321,15 +321,291 @@ def show():
 
     plt.show()
 
+def sprintRecalculate():
+    points = [8,7,6,5,4,3,2,1,0,0,0,0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
+    drivers = []
+    f = open('driverssprint.csv', 'r')
+    lines = f.read().split(',')
+    for d in lines:
+        drivers.append(Driver(d))
+    f.close()
+
+    y = 2021
+    xlabels = [str(y)[-2:]]
+    ny = True
+    f = open('datasprint.csv','r')
+    z = open('winnerssprint.txt', 'w')
+    lines = f.readlines()
+    for n in range(len(lines)):
+        s = lines[n].split(',')
+        p = []
+        if s[0] == "--":
+            maxDiff = ["-",-sys.maxsize-1]
+            minDiff = ["-",-sys.maxsize-1]
+            for i in range(len(drivers)):
+                try:
+                    if s.index(drivers[i].name):
+                        p.append([drivers[i].name, drivers[i].effRating()])
+                        if drivers[i].effRating() - drivers[i].preSeason > maxDiff[1]:
+                            maxDiff = [drivers[i].name, drivers[i].effRating() - drivers[i].preSeason]
+                        if drivers[i].preSeason - drivers[i].effRating() > minDiff[1]:
+                            minDiff = [drivers[i].name, drivers[i].preSeason - drivers[i].effRating()]
+                        drivers[i].preSeason = drivers[i].effRating()
+                except:
+                    continue
+            p = sorted(p,key = lambda x: x[1], reverse=True)
+            z.write(str(y) + ": ")
+            z.write(str(p) + "\n")
+            diff = p[0][1] - p[1][1]
+            z.write("Lead of championship: " + str(diff) + ",\t")
+            diffFromLast = p[0][1] - p[-1][1]
+            z.write("Gap between first and last: " + str(diffFromLast) + ",\t")
+            avgOppRating = 0
+            for i in p:
+                avgOppRating += i[1]
+            avgOppRating -= p[0][1]
+            avgOppRating /= len(p)-1
+            diffFromAvg = p[0][1] - avgOppRating
+            z.write("Gap between first and average rating(not including champion): " + str(diffFromAvg) + ",\t")
+            z.write("Average Rating(not including champion): " + str(avgOppRating) + "\t")
+            z.write(f"Best Performer: {maxDiff[0]}, gained {maxDiff[1]} rating\t")
+            if minDiff[1] < 0:
+                z.write(f"Worst Performer: {minDiff[0]}, gained {-minDiff[1]} rating\t")
+            else:
+                z.write(f"Worst Performer: {minDiff[0]}, lost {minDiff[1]} rating\n\n")
+            y += 1
+            xlabels.append((str(y)[-2:]))
+            for driver in drivers:
+                driver.upload()
+        elif s[0] == "~":
+            for i in range(len(drivers)):
+                try:
+                    if s.index(drivers[i].name):
+                        print(drivers[i].name + " has retired")
+                        drivers[i].upload()
+                        drivers[i].retired = True
+                except:
+                    continue
+        else:
+            if ny:
+                ny = False
+            xlabels.append('')
+            for i in range(len(drivers)):
+                try:
+                    ratings = []
+                    for j in range(len(drivers)):
+                        if j != i:
+                            try:
+                                check = s.index(drivers[j].name)
+                                ratings.append(drivers[j].rating)
+                                n += 1
+                            except:
+                                continue
+                    ratings.sort(reverse=True)
+                    oppAvg = sum(ratings)/len(ratings)
+                    for q in range(len(ratings)):
+                        if ratings[q] <= drivers[i].rating:
+                            break
+                    expected = (((1/(1+(10**((oppAvg - drivers[i].rating)/200))))*2)-1)*8
+                    score = points[s.index(drivers[i].name)]
+                    drivers[i].started = True
+                    drivers[i].races += 1
+                    drivers[i].ratingAdjust(score, expected, 5)
+                    drivers[i].points += score
+                    if score > 0:
+                        drivers[i].championshipPoints += score
+                    if score >= 6:
+                        drivers[i].podiums += 1
+                    if score >= 8:
+                        drivers[i].wins += 1
+                except:
+                    continue
+            for driver in drivers:
+                driver.upload()
+
+    drivers = sorted(drivers, key=lambda x: x.peakRating(), reverse=True)
+    for driver in drivers:
+        print(driver)
+    high = np.nanmax(drivers[0].history)
+    drivers = sorted(drivers, key=lambda x: x.effRating(), reverse=True)
+    fig, ax = plt.subplots()
+    z.close()
+    _names = []
+    _ratingsList = []
+    _ratings = []
+    _started = []
+    _retired = []
+    _races = []
+    _points = []
+    _ppr = []
+    _champpoints = []
+    _champppr = []
+    _wins = []
+    _winspr = []
+    _podiums = []
+    _podiumspr = []
+    for driver in drivers:
+        z = driver.history
+        if driver.started:
+            if not driver.retired:
+                z.append(driver.rating)
+            ax.plot(z, label = driver.name)
+        _names.append(driver.name)
+        _ratingsList.append(z)
+        _ratings.append(driver.effRating())
+        _started.append(driver.started)
+        _retired.append(driver.retired)
+        _races.append(driver.races)
+        _points.append(driver.points)
+        _champpoints.append(driver.championshipPoints)
+        _wins.append(driver.wins)
+        _podiums.append(driver.podiums)
+        if driver.races > 0:
+            _ppr.append(driver.points/driver.races)
+            _champppr.append(driver.championshipPoints/driver.races)
+            _winspr.append(driver.wins/driver.races)
+            _podiumspr.append(driver.podiums/driver.races)
+        else:
+            _ppr.append(0)
+            _champppr.append(0)
+            _winspr.append(0)
+            _podiumspr.append(0)
+
+
+    data = {
+        'Name':_names,
+        'Rating History':_ratingsList,
+        'Rating':_ratings,
+        'Started':_started,
+        'Retired':_retired,
+        'Races':_races,
+        'Points':_points,
+        'Points Per Race':_ppr,
+        'Championship Points':_champpoints,
+        'Championship Points Per Race':_champppr,
+        'Wins':_wins,
+        'Wins Per Race':_winspr,
+        'Podiums':_podiums,
+        'Podiums Per Race':_podiumspr,
+    }
+
+    df = pd.DataFrame(data)
+    df.to_csv('driverDataSprint.csv',index=False)
+
+    xlabelsfile = open('xlabelssprint.csv', 'w')
+    xlabelsfile.write(str(xlabels))
+    xlabelsfile.close()
+
+    plt.xlabel('races')
+    plt.ylabel('elo')
+    ax.set_yticks(np.arange(int(np.nanmin(drivers[-1].history)), int(high), 50))
+    x = np.arange(0,len(xlabels),1)
+    ax.set_xticks(x)
+    ax.set_xticklabels(xlabels)
+    plt.legend(loc="lower right",bbox_to_anchor=(1.1, -0.1), fontsize="5")
+    ax = plt.gca()
+    for tick in ax.get_yticks():
+        ax.axhline(y=tick, color='gray', linestyle='--', linewidth=0.7)
+    for tick, label in zip(x, xlabels):
+        if len(label) > 0:
+            ax.axvline(x=tick, color='gray', linestyle='-', linewidth=1)
+
+    ax.axhline(y=1500, color='black', linestyle='-', linewidth=2)
+    ax.axhline(y=1400, color='black', linestyle='-', linewidth=2)
+    ax.axhline(y=1300, color='black', linestyle='-', linewidth=2)
+
+    plt.show()
+
+    f.close()
+
+def sprintLoad():
+    drivers = []
+    df = pd.read_csv('driverDataSprint.csv')
+    for index, row in df.iterrows():
+        driver = Driver(row['Name'], row['Rating'], row['Started'], row['Retired'], row['Races'], row['Points'], row['Championship Points'], row['Wins'], row['Podiums'])
+        list = row['Rating History'][1:-1].split(', ')
+        history = []
+        for i in list:
+            if i == "nan":
+                history.append(np.nan)
+            else:
+                history.append(float(i))
+        driver.addHistory(history)
+        drivers.append(driver)
+    return drivers
+
+def sprintShow():
+    drivers = load()
+    xlabelsfile = open('xlabelssprint.csv', 'r')
+    xlabels = xlabelsfile.read()
+    xlabelsfile.close()
+    
+    xlabels = xlabels[1:-1].split(', ')
+    for i in range(len(xlabels)):
+        xlabels[i] = xlabels[i][1:-1]
+
+    drivers = sorted(drivers, key=lambda x: x.peakRating(), reverse=True)
+    for driver in drivers:
+        print(driver)
+    high = np.nanmax(drivers[0].history)
+    drivers = sorted(drivers, key=lambda x: x.effRating(), reverse=True)
+    fig, ax = plt.subplots()
+
+    toViewFile = open("toView.csv", "r")
+    toView = toViewFile.read().split(',')
+    toViewFile.close()
+    if len(toView) == 1 and toView[0] == '':
+        for driver in drivers:
+            z = driver.history
+            if driver.started:
+                if not driver.retired:
+                    z.append(driver.rating)
+                ax.plot(z, label = driver.name)
+    else:
+        for driver in drivers:
+            if driver.name in toView:
+                z = driver.history
+                if driver.started:
+                    if not driver.retired:
+                        z.append(driver.rating)
+                    ax.plot(z, label = driver.name)
+
+    plt.xlabel('races')
+    plt.ylabel('elo')
+    ax.set_yticks(np.arange(int(np.nanmin(drivers[-1].history)), int(high), 50))
+    x = np.arange(0,len(xlabels),1)
+    ax.set_xticks(x)
+    ax.set_xticklabels(xlabels)
+    plt.legend(loc="lower right",bbox_to_anchor=(1.1, -0.1), fontsize="5")
+    ax = plt.gca()
+
+    for tick in ax.get_yticks():
+        ax.axhline(y=tick, color='gray', linestyle='--', linewidth=0.7)
+    for tick, label in zip(x, xlabels):
+        if len(label) > 1:
+            ax.axvline(x=tick, color='gray', linestyle='-', linewidth=1)
+
+    ax.axhline(y=1500, color='black', linestyle='-', linewidth=2)
+    ax.axhline(y=1400, color='black', linestyle='-', linewidth=2)
+    ax.axhline(y=1300, color='black', linestyle='-', linewidth=2)
+
+    plt.show()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Recalculate or load the results")
     parser.add_argument("-c", "--calculate", action="store_true", help="Recalculate ratings")
     parser.add_argument("-l", "--load", action="store_true", help="Load previously calculated ratings")
+    parser.add_argument("-cs", "--calculate_sprint", action="store_true", help="Recalculate sprint ratings")
+    parser.add_argument("-ls", "--load_sprint", action="store_true", help="Load previously calculated sprint ratings")
 
     args = parser.parse_args()
 
     if args.load:
         show()
+    if args.load_sprint:
+        sprintShow()
+    if args.calculate_sprint:
+        sprintRecalculate()
     elif args.calculate or not any(vars(args).values()):
         recalculate()
